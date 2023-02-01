14bd45396fb1f0589294f2319007503669c68086
MuhamedShahabas <muhamedshahabasc@gmail.com>
2023-02-01 17:28:48 +0530

    Added FilterChart

--- src/App.js
+++ src/App.js
@@ -1,37 +1,45 @@
+import { useEffect, useState } from "react";
 import Expenses from "./components/Expenses/Expenses";
 import NewExpense from "./components/NewExpense/NewExpense";
 
+const DUMMY_EXPENSES = [
+  {
+    id: "e1",
+    title: "Vacation",
+    amount: 5000,
+    date: new Date(2020, 7, 14),
+  },
+  { id: "e2", title: "TV", amount: 1500, date: new Date(2021, 2, 12) },
+  {
+    id: "e3",
+    title: "Bike Tyre",
+    amount: 4800,
+    date: new Date(2022, 2, 28),
+  },
+  {
+    id: "e4",
+    title: "Laptop Keyboard",
+    amount: 4500,
+    date: new Date(2023, 1, 12),
+  },
+];
+
 const App = () => {
-  const expenses = [
-    {
-      id: "e1",
-      title: "Toilet Paper",
-      amount: 94.12,
-      date: new Date(2020, 7, 14),
-    },
-    { id: "e2", title: "New TV", amount: 799.49, date: new Date(2021, 2, 12) },
-    {
-      id: "e3",
-      title: "Car Insurance",
-      amount: 294.67,
-      date: new Date(2021, 2, 28),
-    },
-    {
-      id: "e4",
-      title: "New Desk (Wooden)",
-      amount: 450,
-      date: new Date(2021, 5, 12),
-    },
-  ];
+  useEffect(() => {
+    document.title = "Yearly Expense";
+  }, []);
+  
+  const [expenses, setExpenses] = useState(DUMMY_EXPENSES);
 
   const addExpenseHandler = (expense) => {
-    console.log("In app.js");
-    console.log(expense);
+    setExpenses((prevExpense) => {
+      return [expense, ...prevExpense];
+    });
   };
 
   return (
     <div>
-      <h2>Let's get started!!</h2>
+      <h2 style={{ "text-align": "center", color: "white" }}>Yearly Expense</h2>
       <NewExpense onAddExpense={addExpenseHandler} />
       <Expenses items={expenses} />
     </div>
@@ -39,3 +47,13 @@ const App = () => {
 };
 
 export default App;
+
+// useEffect(() => {
+//   localStorage.setItem("Yearly-Expense", JSON.stringify(DUMMY_EXPENSES));
+// }, [DUMMY_EXPENSES]);
+
+// const getLocalStorageItems = () => {
+//   const savedData = localStorage.getItem("Yearly-Expense");
+//   const initialValue = JSON.parse(savedData);
+//   return initialValue || DUMMY_EXPENSES;
+// };
--- src/components/Expenses/ExpenseFilter.js
+++ src/components/Expenses/ExpenseFilter.js
@@ -3,13 +3,13 @@ import './ExpenseFilter.css';
 
 const ExpensesFilter = (props) => {
     const dropdownChangeHandler = (event) => {
-        props.onExpenseFilter(event.target.value)
+        props.onChangeFilter(event.target.value)
     }
   return (
     <div className='expenses-filter'>
       <div className='expenses-filter__control'>
         <label>Filter by year</label>
-        <select onChange={dropdownChangeHandler}>
+        <select value={props.selected} onChange={dropdownChangeHandler}>
           <option value='2019'>2023</option>
           <option value='2022'>2022</option>
           <option value='2021'>2021</option>
